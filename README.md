54fcf150fbf05b1e02798322cbc82943d1c8a77c
irshadvali <irshad.vali@gmail.com>
2018-06-30 20:58:48 +0530

    read me

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # npm-all-area
+# How to use
+const {circleArea,circlePerimeter,squareArea,squarePerimeter,rectangleArea,rectanglePerimeter}=require(npm-all-are)
+or
+import {circleArea,circlePerimeter,squareArea,squarePerimeter,rectangleArea,rectanglePerimeter} from "all"
---
+++ index.js
@@ -0,0 +1,6 @@
+module.exports.circleArea=radius=>Math.PI.radius.radius
+module.exports.circlePerimeter=radius=>2*Math.PI.radius
+module.exports.squareArea=side=>side*side
+module.exports.squarePerimeter=side=>4*side
+module.exports.rectangleArea=(length,width)=>length*width
+module.exports.rectanglePerimeter=(length,width)=>2*(length+width)
